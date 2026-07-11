fb60b5d05cd4e2ca61d825cc906658d711080e88
betpros <mosesgich10@gmail.com>
2026-07-11 11:45:13 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@
    Leave as null to run the site in demo mode (fake, simulated payments,
    nothing charged, nothing sent to a server).
    Once your backend is deployed on Render, set this to its URL, e.g.:
-     window.BETPRO_API_BASE = "https://betpro-backend.onrender.com";
+     window.BETPRO_API_BASE = "https://btpro-6vqw.onrender.com";
 */
 window.BETPRO_API_BASE = https://btpro-6vqw.onrender.com;
